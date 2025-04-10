ba1c3689ad6cb87b6a2cad8923fa1d594fdf3fb7
TKhoi <153786779+TuanKhoi67@users.noreply.github.com>
2025-04-11 04:08:24 +0700

    Update app.js

--- app.js
+++ app.js
@@ -208,7 +208,7 @@ app.use((err, req, res, next) => {
 
 
 // 🚀 **Chạy server**
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 httpServer.listen(PORT, () => {
   console.log(`🚀 Server is running on port ${PORT}`);
 });
