af91e5cdcd79824f8813a57175b0339a5fc16eeb
devtran211 <141512167+devtran211@users.noreply.github.com>
2025-04-17 21:00:05 +0700

    Update Tutor.js

--- models/Tutor.js
+++ models/Tutor.js
@@ -20,4 +20,13 @@ var TutorSchema = mongoose.Schema({
   }
 });
 
-module.exports = mongoose.model('Tutor', TutorSchema);
+TutorSchema.methods.formatEnrollmentDate = function () {
+      const date = this.enrollmentDate;
+      const day = String(date.getDate()).padStart(2, '0');
+      const month = String(date.getMonth() + 1).padStart(2, '0'); // Tháng bắt đầu từ 0
+      const year = date.getFullYear();
+      return `${day}/${month}/${year}`;
+};
+
+var TutorModel = mongoose.model('tutors', TutorSchema);
+module.exports = TutorModel;
