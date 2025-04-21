920008debfdc908c5621e704a8b44a06c8508cb7
devtran211 <141512167+devtran211@users.noreply.github.com>
2025-04-21 21:33:09 +0700

    Update Meeting.js

--- models/Meeting.js
+++ models/Meeting.js
@@ -24,8 +24,20 @@ var MeetingSchema = mongoose.Schema({
         type: Date,  // Thời gian kết thúc cuộc họp
         required: true 
    },
-   note: String
+   note: String,
+   createDate: {
+     type: Date,
+      default: Date.now
+     },
 });
+
+MeetingSchema.methods.formatEnrollmentDate = function () {
+     const date = this.enrollmentDate;
+     const day = String(date.getDate()).padStart(2, '0');
+     const month = String(date.getMonth() + 1).padStart(2, '0'); // Tháng bắt đầu từ 0
+     const year = date.getFullYear();
+     return `${day}/${month}/${year}`;
+  };
 
 var MeetingModel = mongoose.model('meetings', MeetingSchema);
 module.exports = MeetingModel;
